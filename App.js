b9585ff15b5edc8a7177852ffe0a70cb1554e812
acee3 <pgaa134@gmail.com>
2021-03-25 20:34:35 -0400

    Small Updates to App.js and Tabs.js

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
-import { Text, View, Image, StyleSheet, ScrollView, Alert } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './components/Tabs';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, bottom: 0 }}>
-      <NavigationContainer style={{ flex: 1, bottom: 0 }}>
-        <Tabs/>
-      </NavigationContainer>
-    </View>
+    <Tabs/>
   );
 }
-
--- components/Tabs.js
+++ components/Tabs.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { MaterialCommunityIcons, Feather, FontAwesome } from '@expo/vector-icons';
 
 import Services from "./Services.js";
@@ -11,55 +12,57 @@ const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
   return (
-    <Tab.Navigator
-      initialRouteName="Services"
-      tabBarOptions={{
-        activeTintColor: '#a83232',
-      }}>
-      <Tab.Screen
-        name="Services"
-        component={Services}
-        options={{
-          tabBarLabel: 'Services',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="hammer-wrench"
-              color={color}
-              size={size}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Checkout"
-        component={Checkout}
-        options={{
-          tabBarLabel: 'Checkout',
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="shopping-cart" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Orders"
-        component={Orders}
-        options={{
-          tabBarLabel: 'Orders',
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="list" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Account"
-        component={Account}
-        options={{
-          tabBarLabel: 'Account',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <NavigationContainer style={{ flex: 1, bottom: 0, overflow: 'hidden' }}>
+      <Tab.Navigator
+        initialRouteName="Services"
+        tabBarOptions={{
+          activeTintColor: '#a83232',
+        }}>
+        <Tab.Screen
+          name="Services"
+          component={Services}
+          options={{
+            tabBarLabel: 'Services',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="hammer-wrench"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Checkout"
+          component={Checkout}
+          options={{
+            tabBarLabel: 'Checkout',
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="shopping-cart" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Orders"
+          component={Orders}
+          options={{
+            tabBarLabel: 'Orders',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="list" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={Account}
+          options={{
+            tabBarLabel: 'Account',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
