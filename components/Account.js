9a5151dd7b6802f5c7172ad0e00d94a105383497
kliu23 <59066393+kliu23@users.noreply.github.com>
2021-03-27 22:04:11 -0400

    Update Account.js

--- components/Account.js
+++ components/Account.js
@@ -5,14 +5,14 @@ import fire from '../fire';
 
 const Account = (props) => {
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={styles.scrollView}>
       <View style={styles.container}>
         <Image
           source={{ uri: 'https://i.imgur.com/ZQgJDK1.jpg' }}
           style={styles.logo}
         />
       </View>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 50, marginBottom: 50 }}>
         <Button 
           title='Logout'
           onPress={props.handleLogout}
@@ -25,6 +25,12 @@ const Account = (props) => {
 export default Account;
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -34,5 +40,6 @@ const styles = StyleSheet.create({
     width: 625,
     height: 180,
     marginBottom: 10,
+    marginTop: 10,
   },
 });
