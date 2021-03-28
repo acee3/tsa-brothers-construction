d6f4a4a10d7489b1b17134e903af613f4148cb9d
kliu23 <59066393+kliu23@users.noreply.github.com>
2021-03-27 22:04:39 -0400

    Update Orders.js

--- components/Orders.js
+++ components/Orders.js
@@ -28,47 +28,59 @@ export default function Orders() {
   }, []);
 
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={styles.scrollView}>
       <View style={styles.container}>
         <Image
           source={{ uri: 'https://i.imgur.com/ZQgJDK1.jpg' }}
           style={styles.logo}
         />
       </View>
-      <List.Section title="Orders">
-        {
-          orders.map((item, i) => (
-            <View>
-              <List.Accordion
-                key={i}
-                title={item.date.substring(0, 3) + ', ' + item.date.substring(4, 10) + ', ' + item.date.substring(11, 15) + ' at ' + item.date.substring(16, 24)}
-                description={'$' + item.total}
-                left={props => <AntDesign name="check" size={24} color="black" />}>
-                {item.services.map((item2) => (
-                  <List.Item title={item2.productName} />
-                ))}
-              </List.Accordion>
-              <Button
-                title='Cancel'
-                buttonStyle={{ backgroundColor: 'red' }}
-                onPress={() => {
-                  fire.firestore()
-                    .collection('users')
-                    .doc(fire.auth().currentUser.uid)
-                    .collection('orders')
-                    .doc(ids[i])
-                    .delete()
-                }}
-              />
-            </View>
-          ))
-        }
-      </List.Section>
+      <Card containerStyle={{ marginBottom: 70 }}>
+        <Card.Title style={styles.subTitleText}>Past Orders</Card.Title>
+        <List.Section>
+          {
+            orders.map((item, i) => (
+              <Card>
+                <View>
+                  <List.Accordion
+                    key={i}
+                    title={item.date.substring(0, 3) + ', ' + item.date.substring(4, 10) + ', ' + item.date.substring(11, 15) + ' at ' + item.date.substring(16, 24)}
+                    description={'$' + item.total}
+                    left={props => <AntDesign name="check" size={24} color="black" />}
+                    bottomDivider>
+                    {item.services.map((item2) => (
+                      <List.Item title={item2.productName} style={styles.item} />
+                    ))}
+                  </List.Accordion>
+                  <Button
+                    title='Cancel'
+                    buttonStyle={{ backgroundColor: 'red' }}
+                    onPress={() => {
+                      fire.firestore()
+                        .collection('users')
+                        .doc(fire.auth().currentUser.uid)
+                        .collection('orders')
+                        .doc(ids[i])
+                        .delete()
+                    }}
+                  />
+                </View>
+              </Card>
+            ))
+          }
+        </List.Section>
+      </Card>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -78,5 +90,6 @@ const styles = StyleSheet.create({
     width: 625,
     height: 180,
     marginBottom: 10,
+    marginTop: 10,
   },
 });
